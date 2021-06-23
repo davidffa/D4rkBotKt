bc907a09ff584a0bf48ee7aff81eef5d9de7d884
David Amorim <davidfilipeamorim@gmail.com>
2021-06-23 13:14:23 +0100

    Change botinfo embed

--- src/main/kotlin/net/d4rkb/d4rkbotkt/commands/info/Botinfo.kt
+++ src/main/kotlin/net/d4rkb/d4rkbotkt/commands/info/Botinfo.kt
@@ -24,15 +24,21 @@ class Botinfo : Command(
         val embed = EmbedBuilder()
             .setTitle("<a:blobdance:804026401849475094> Informações sobre mim")
             .setColor(Utils.randColor())
+            .setDescription("**[Convite](https://discord.com/oauth2/authorize?client_id=${ctx.jda.selfUser.id}&scope=bot&permissions=8)**\n" +
+                    "**[Servidor de Suporte](https://discord.gg/dBQnxVCTEw)**\n\n")
             .addField(":id: Meu ID", "`${ctx.selfUser.id}`", true)
             .addField(":man: Dono", "`D4rkB#2408`", true)
             .addField(":calendar: Criado em", "`${Utils.formatDate(ctx.selfUser.timeCreated)}`", true)
-            .addField("<a:infinity:838759634361253929> Uptime", "`${Utils.msToDate(ManagementFactory.getRuntimeMXBean().uptime)}`", true)
+            .addField("<a:infinity:838759634361253929> Uptime",
+                "`${Utils.msToDate(ManagementFactory.getRuntimeMXBean().uptime)}`",
+                true)
             .addField(":desktop: Servidores", "`${ctx.jda.guilds.size}`", true)
-            .addField(":ping_pong: Ping", "`${ctx.jda.gatewayPing}`", true)
-            .addField("<:badgehypesquad:803665497223987210> Prefixos", "`dk.`", true)
-            .addField("<:ram:751468688686841986> RAM", "Alocada: `${runtime.totalMemory() / 1024 / 1024}MB`\n" +
-                    "Usada: `${(runtime.totalMemory() - runtime.freeMemory()) / 1024 / 1024}MB`", true)
+            .addField(":ping_pong: Ping", "`${ctx.jda.gatewayPing}ms`", true)
+            .addField("<:badgehypesquad:803665497223987210> Prefixos", "Padrão: `dk.`", true)
+            .addField("<:ram:751468688686841986> RAM",
+                "Usada: `${(runtime.totalMemory() - runtime.freeMemory()) / 1024 / 1024}MB`\n" +
+                        "Alocada: `${runtime.totalMemory() / 1024 / 1024}MB`",
+                true)
             .addField("<:kotlin:856168010004037702> Versões:", "Kotlin: `${KotlinVersion.CURRENT}`\n" +
                     "JVM: `${System.getProperty("java.version")}`\n" +
                     "JDA: `${JDAInfo.VERSION}`\n" +
