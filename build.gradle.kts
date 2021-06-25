b9c20f99e8efda006bf8b67e008936bd75c6abd7
David Amorim <davidfilipeamorim@gmail.com>
2021-06-25 14:46:28 +0100

    Update kotlin to v1.5.20

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,7 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
 
 plugins {
-    kotlin("jvm") version "1.5.10"
+    kotlin("jvm") version "1.5.20"
     id("com.github.johnrengelman.shadow") version "7.0.0"
 }
 
