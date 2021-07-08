4cf0b4ee712076bcd305088568ddec76222497b2
David Amorim <davidfilipeamorim@gmail.com>
2021-07-08 17:18:17 +0100

    Implement selection menu on filters cmd

--- src/main/kotlin/me/davidffa/d4rkbotkt/commands/music/Filters.kt
+++ src/main/kotlin/me/davidffa/d4rkbotkt/commands/music/Filters.kt
@@ -1,19 +1,18 @@
 package me.davidffa.d4rkbotkt.commands.music
 
-import dev.minn.jda.ktx.CoroutineEventListener
-import dev.minn.jda.ktx.await
-import dev.minn.jda.ktx.onButton
+import dev.minn.jda.ktx.*
+import dev.minn.jda.ktx.interactions.SelectionMenu
+import dev.minn.jda.ktx.interactions.option
 import me.davidffa.d4rkbotkt.audio.GuildMusicManager
 import me.davidffa.d4rkbotkt.command.Command
 import me.davidffa.d4rkbotkt.command.CommandContext
 import me.davidffa.d4rkbotkt.audio.PlayerManager
 import me.davidffa.d4rkbotkt.audio.filters.Filter
 import me.davidffa.d4rkbotkt.utils.Utils
-import net.dv8tion.jda.api.EmbedBuilder
 import net.dv8tion.jda.api.JDA
 import net.dv8tion.jda.api.Permission
 import net.dv8tion.jda.api.entities.Emoji
-import net.dv8tion.jda.api.events.interaction.ButtonClickEvent
+import net.dv8tion.jda.api.events.interaction.GenericComponentInteractionCreateEvent
 import net.dv8tion.jda.api.interactions.components.ActionRow
 import net.dv8tion.jda.api.interactions.components.Button
 import java.security.SecureRandom
@@ -43,32 +42,48 @@ class Filters : Command(
         SecureRandom().nextBytes(nonceBytes)
         val nonce = Base64.getEncoder().encodeToString(nonceBytes)
 
-        val bassButton = Button.success("$nonce:bass", Emoji.fromUnicode("1️⃣"))
-        val dcButton = Button.success("$nonce:daycore", Emoji.fromUnicode("2️⃣"))
-        val ncButton = Button.success("$nonce:nightcore", Emoji.fromUnicode("3️⃣"))
-        val popButton = Button.success("$nonce:pop", Emoji.fromUnicode("4️⃣"))
-        val softButton = Button.success("$nonce:soft", Emoji.fromUnicode("5️⃣"))
-        val trebleButton = Button.success("$nonce:treble", Emoji.fromUnicode("6️⃣"))
-        val vaporwaveButton = Button.success("$nonce:vaporwave", Emoji.fromUnicode("7️⃣"))
-        val eightd = Button.success("$nonce:8d", Emoji.fromUnicode("8️⃣"))
-        val vibratoButton = Button.success("$nonce:vibrato", Emoji.fromUnicode("9️⃣"))
-        val vocalsButton = Button.success("$nonce:vocals", Emoji.fromUnicode("\uD83D\uDD1F"))
-        val karaokeButton = Button.success("$nonce:karaoke", Emoji.fromMarkdown("<:keycap_11:862005088716193793>"))
+        val menu = SelectionMenu("$nonce:filters", "Escolhe um filtro para ativar/desativar") {
+            option("Bass", "bass", emoji = Emoji.fromUnicode("1️⃣"))
+            option("Daycore", "daycore", emoji = Emoji.fromUnicode("2️⃣"))
+            option("Nightcore", "nightcore", emoji = Emoji.fromUnicode("3️⃣"))
+            option("Pop", "pop", emoji = Emoji.fromUnicode("4️⃣"))
+            option("Soft", "soft", emoji = Emoji.fromUnicode("5️⃣"))
+            option("Treblebass", "treble", emoji = Emoji.fromUnicode("6️⃣"))
+            option("Vaporwave", "vaporwave", emoji = Emoji.fromUnicode("7️⃣"))
+            option("8D", "8d", emoji = Emoji.fromUnicode("8️⃣"))
+            option("Vibrato", "vibrato", emoji = Emoji.fromUnicode("9️⃣"))
+            option("Vocals", "vocals", emoji = Emoji.fromUnicode("\uD83D\uDD1F"))
+            option("Karaoke", "karaoke", emoji = Emoji.fromMarkdown("<:keycap_11:862005088716193793>"))
+        }
+
         val clearButton = Button.danger("$nonce:clear", Emoji.fromUnicode("\uD83D\uDDD1️"))
         val deleteButton = Button.danger("$nonce:close", "Fechar")
 
-        val embed = EmbedBuilder()
-            .setTitle(":level_slider: Mesa de DJ")
-            .setDescription(getDescription(manager.filters))
-            .setColor(Utils.randColor())
-            .setThumbnail("https://i.pinimg.com/564x/a3/a9/29/a3a929cc8d09e88815b89bc071ff4d8d.jpg")
-            .setFooter(ctx.author.asTag, ctx.author.effectiveAvatarUrl)
-            .setTimestamp(Instant.now())
+        val embed = EmbedBuilder {
+            title = ":level_slider: Mesa de DJ"
+            color = Utils.randColor()
+            description = ":wastebasket: - Remove todos os filtros"
+            thumbnail = "https://i.pinimg.com/564x/a3/a9/29/a3a929cc8d09e88815b89bc071ff4d8d.jpg"
+            footer {
+                name = ctx.author.asTag
+                iconUrl = ctx.author.effectiveAvatarUrl
+            }
+            timestamp = Instant.now()
+        }
+
+        val desc = getDescription(listOf())
+        embed.field {
+            name = "\u200B"
+            value = desc.first
+        }
+        embed.field {
+            name = "\u200B"
+            value = desc.second
+        }
 
         val controls = listOf(
-            ActionRow.of(bassButton, dcButton, ncButton, popButton, softButton),
-            ActionRow.of(trebleButton, vaporwaveButton, eightd, vibratoButton, vocalsButton),
-            ActionRow.of(karaokeButton, clearButton, deleteButton)
+            ActionRow.of(menu),
+            ActionRow.of(clearButton, deleteButton)
         )
 
         val msg = ctx.channel.sendMessageEmbeds(embed.build())
@@ -83,61 +98,26 @@ class Filters : Command(
             close(ctx.jda, listeners, manager)
         }, 60000)
 
-        val bassListener = ctx.jda.onButton("$nonce:bass") {
-            if (it.member?.idLong != ctx.member.idLong) return@onButton
-            manager.switchFilter(Filter.BASS)
+        val menuListener = ctx.jda.onSelection("$nonce:filters") {
+            val selection = it.selectedOptions?.first()!!
+            if (it.member?.idLong != ctx.member.idLong) return@onSelection
+
+            when (selection.value) {
+                "bass" -> manager.switchFilter(Filter.BASS)
+                "daycore" -> manager.switchFilter(Filter.DAYCORE)
+                "nightcore" -> manager.switchFilter(Filter.NIGHTCORE)
+                "pop" -> manager.switchFilter(Filter.POP)
+                "soft" -> manager.switchFilter(Filter.SOFT)
+                "treble" -> manager.switchFilter(Filter.TREBLEBASS)
+                "vaporwave" -> manager.switchFilter(Filter.VAPORWAVE)
+                "vibrato" -> manager.switchFilter(Filter.VIBRATO)
+                "vocals" -> manager.switchFilter(Filter.VOCALS)
+                "karaoke" -> manager.switchFilter(Filter.KARAOKE)
+            }
+
             editMessage(it, embed, manager.filters)
         }
-        val dcListener = ctx.jda.onButton("$nonce:daycore") {
-            if (it.member?.idLong != ctx.member.idLong) return@onButton
-            manager.switchFilter(Filter.DAYCORE)
-            editMessage(it, embed, manager.filters)
-        }
-        val ncListener = ctx.jda.onButton("$nonce:nightcore") {
-            if (it.member?.idLong != ctx.member.idLong) return@onButton
-            manager.switchFilter(Filter.NIGHTCORE)
-            editMessage(it, embed, manager.filters)
-        }
-        val popListener = ctx.jda.onButton("$nonce:pop") {
-            if (it.member?.idLong != ctx.member.idLong) return@onButton
-            manager.switchFilter(Filter.POP)
-            editMessage(it, embed, manager.filters)
-        }
-        val softListener = ctx.jda.onButton("$nonce:soft") {
-            if (it.member?.idLong != ctx.member.idLong) return@onButton
-            manager.switchFilter(Filter.SOFT)
-            editMessage(it, embed, manager.filters)
-        }
-        val trebleListener = ctx.jda.onButton("$nonce:treble") {
-            if (it.member?.idLong != ctx.member.idLong) return@onButton
-            manager.switchFilter(Filter.TREBLEBASS)
-            editMessage(it, embed, manager.filters)
-        }
-        val vaporwaveListener = ctx.jda.onButton("$nonce:vaporwave") {
-            if (it.member?.idLong != ctx.member.idLong) return@onButton
-            manager.switchFilter(Filter.VAPORWAVE)
-            editMessage(it, embed, manager.filters)
-        }
-        val vibratoListener = ctx.jda.onButton("$nonce:vibrato") {
-            if (it.member?.idLong != ctx.member.idLong) return@onButton
-            manager.switchFilter(Filter.VIBRATO)
-            editMessage(it, embed, manager.filters)
-        }
-        val vocalsListener = ctx.jda.onButton("$nonce:vocals") {
-            if (it.member?.idLong != ctx.member.idLong) return@onButton
-            manager.switchFilter(Filter.VOCALS)
-            editMessage(it, embed, manager.filters)
-        }
-        val eightdListener = ctx.jda.onButton("$nonce:8d") {
-            if (it.member?.idLong != ctx.member.idLong) return@onButton
-            manager.switchFilter(Filter.EIGHTD)
-            editMessage(it, embed, manager.filters)
-        }
-        val karaokeListener = ctx.jda.onButton("$nonce:karaoke") {
-            if (it.member?.idLong != ctx.member.idLong) return@onButton
-            manager.switchFilter(Filter.KARAOKE)
-            editMessage(it, embed, manager.filters)
-        }
+
         val clearListener = ctx.jda.onButton("$nonce:clear") {
             if (it.member?.idLong != ctx.member.idLong) return@onButton
             manager.clearFilters()
@@ -150,17 +130,7 @@ class Filters : Command(
             it.deferEdit().queue()
         }
 
-        listeners.add(bassListener)
-        listeners.add(dcListener)
-        listeners.add(ncListener)
-        listeners.add(popListener)
-        listeners.add(softListener)
-        listeners.add(trebleListener)
-        listeners.add(vaporwaveListener)
-        listeners.add(vibratoListener)
-        listeners.add(vocalsListener)
-        listeners.add(eightdListener)
-        listeners.add(karaokeListener)
+        listeners.add(menuListener)
         listeners.add(clearListener)
         listeners.add(closeListener)
     }
@@ -180,24 +150,35 @@ class Filters : Command(
         manager.djtableMessage = null
     }
 
-    private fun editMessage(event: ButtonClickEvent, embed: EmbedBuilder, filters: List<Filter>) {
-        embed.setDescription(getDescription(filters))
+    private fun editMessage(event: GenericComponentInteractionCreateEvent, embed: InlineEmbed, filters: List<Filter>) {
+        val desc = getDescription(filters)
+        embed.builder.clearFields()
+        embed.field {
+            name = "\u200B"
+            value = desc.first
+        }
+        embed.field {
+            name = "\u200B"
+            value = desc.second
+        }
         event.editMessageEmbeds(embed.build())
             .queue()
     }
 
-    private fun getDescription(filters: List<Filter>): String {
-        return  "**1)** Bass ${if (filters.contains(Filter.BASS)) "<:on:764478511875751937>" else "<:off:764478504124416040>"}\n" +
-                "**2)** Daycore ${if (filters.contains(Filter.DAYCORE)) "<:on:764478511875751937>" else "<:off:764478504124416040>"}\n" +
-                "**3)** Nightcore ${if (filters.contains(Filter.NIGHTCORE)) "<:on:764478511875751937>" else "<:off:764478504124416040>"}\n" +
-                "**4)** Pop ${if (filters.contains(Filter.POP)) "<:on:764478511875751937>" else "<:off:764478504124416040>"}\n" +
-                "**5)** Soft ${if (filters.contains(Filter.SOFT)) "<:on:764478511875751937>" else "<:off:764478504124416040>"}\n" +
-                "**6)** Treblebass ${if (filters.contains(Filter.TREBLEBASS)) "<:on:764478511875751937>" else "<:off:764478504124416040>"}\n" +
-                "**7)** Vaporwave ${if (filters.contains(Filter.VAPORWAVE)) "<:on:764478511875751937>" else "<:off:764478504124416040>"}\n" +
-                "**8)** 8D ${if (filters.contains(Filter.EIGHTD)) "<:on:764478511875751937>" else "<:off:764478504124416040>"}\n" +
-                "**9)** Vibrato ${if (filters.contains(Filter.VIBRATO)) "<:on:764478511875751937>" else "<:off:764478504124416040>"}\n" +
-                "**10)** Vocals ${if (filters.contains(Filter.VOCALS)) "<:on:764478511875751937>" else "<:off:764478504124416040>"}\n" +
-                "**11)** Karaoke ${if (filters.contains(Filter.KARAOKE)) "<:on:764478511875751937>" else "<:off:764478504124416040>"}\n\n" +
-                ":wastebasket: - Remove todos os filtros"
+    private fun getDescription(filters: List<Filter>): Pair<String, String> {
+        val first = "Bass ${if (filters.contains(Filter.BASS)) "<:on:764478511875751937>" else "<:off:764478504124416040>"}\n" +
+                "Daycore ${if (filters.contains(Filter.DAYCORE)) "<:on:764478511875751937>" else "<:off:764478504124416040>"}\n" +
+                "Nightcore ${if (filters.contains(Filter.NIGHTCORE)) "<:on:764478511875751937>" else "<:off:764478504124416040>"}\n" +
+                "Pop ${if (filters.contains(Filter.POP)) "<:on:764478511875751937>" else "<:off:764478504124416040>"}\n" +
+                "Soft ${if (filters.contains(Filter.SOFT)) "<:on:764478511875751937>" else "<:off:764478504124416040>"}\n" +
+                "Treblebass ${if (filters.contains(Filter.TREBLEBASS)) "<:on:764478511875751937>" else "<:off:764478504124416040>"}"
+
+        val second = "Vaporwave ${if (filters.contains(Filter.VAPORWAVE)) "<:on:764478511875751937>" else "<:off:764478504124416040>"}\n" +
+                "8D ${if (filters.contains(Filter.EIGHTD)) "<:on:764478511875751937>" else "<:off:764478504124416040>"}\n" +
+                "Vibrato ${if (filters.contains(Filter.VIBRATO)) "<:on:764478511875751937>" else "<:off:764478504124416040>"}\n" +
+                "Vocals ${if (filters.contains(Filter.VOCALS)) "<:on:764478511875751937>" else "<:off:764478504124416040>"}\n" +
+                "Karaoke ${if (filters.contains(Filter.KARAOKE)) "<:on:764478511875751937>" else "<:off:764478504124416040>"}"
+
+        return Pair(first, second)
     }
 }
