11fae9a5d722e3e65820116dbaea01f3828a6965
David Amorim <davidfilipeamorim@gmail.com>
2021-08-07 20:28:33 +0100

    Improve bass filter

--- src/main/kotlin/me/davidffa/d4rkbotkt/audio/filters/configs/Bass.kt
+++ src/main/kotlin/me/davidffa/d4rkbotkt/audio/filters/configs/Bass.kt
@@ -6,10 +6,10 @@ import com.sedmelluq.discord.lavaplayer.format.AudioDataFormat
 
 fun bass(format: AudioDataFormat, output: FloatPcmAudioFilter): List<FloatPcmAudioFilter> {
   val bands = FloatArray(Equalizer.BAND_COUNT) { 0f }
-  bands[0] = 0.7f
-  bands[1] = 0.6f
-  bands[2] = 0.5f
-  bands[3] = 0.31f
+  bands[0] = 0.5f
+  bands[1] = 0.3f
+  bands[2] = 0.2f
+  bands[3] = 0.18f
 
   return listOf(Equalizer(format.channelCount, output, bands))
 }
