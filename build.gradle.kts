2bdd2b0b26f2d48ed39fc18cc32a255bc853e5d8
David Amorim <davidfilipeamorim@gmail.com>
2021-10-03 18:57:48 +0100

    Update lavaplayer

--- build.gradle.kts
+++ build.gradle.kts
@@ -34,7 +34,7 @@ dependencies {
     exclude("opus-java")
   }
   implementation("com.github.minndevelopment:jda-ktx:d90709a")
-  implementation("com.github.davidffa:lavaplayer-fork:c0b7346")
+  implementation("com.github.davidffa:lavaplayer-fork:ea562b1")
   implementation("com.sedmelluq:jda-nas:1.1.0")
 
   // Audio converter
