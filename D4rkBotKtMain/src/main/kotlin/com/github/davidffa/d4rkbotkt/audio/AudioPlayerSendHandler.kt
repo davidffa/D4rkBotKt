9db0c41fac7beefc8f15a6fe9fbee7a9cbc45f64
David Amorim <davidfilipeamorim@gmail.com>
2024-04-28 02:09:10 +0100

    Allocate a bigger audio buffer

--- D4rkBotKtMain/src/main/kotlin/com/github/davidffa/d4rkbotkt/audio/AudioPlayerSendHandler.kt
+++ D4rkBotKtMain/src/main/kotlin/com/github/davidffa/d4rkbotkt/audio/AudioPlayerSendHandler.kt
@@ -1,12 +1,13 @@
 package com.github.davidffa.d4rkbotkt.audio
 
+import com.sedmelluq.discord.lavaplayer.format.StandardAudioDataFormats
 import com.sedmelluq.discord.lavaplayer.player.AudioPlayer
 import com.sedmelluq.discord.lavaplayer.track.playback.MutableAudioFrame
 import net.dv8tion.jda.api.audio.AudioSendHandler
 import java.nio.ByteBuffer
 
 class AudioPlayerSendHandler(private val audioPlayer: AudioPlayer) : AudioSendHandler {
-  private val buffer = ByteBuffer.allocate(1024)
+  private val buffer = ByteBuffer.allocate(StandardAudioDataFormats.DISCORD_OPUS.maximumChunkSize())
   private val frame = MutableAudioFrame()
 
   init {
